08994e531ac92c1b2653e3878971576acf5d2d3f
Asimile <AsimNRaja@gmail.com>
2024-10-30 12:03:45 -0400

    Created playground and click input
    Made a playground to be used for future testing of the grappling hook feature, as well as wrote the rudimentary logic for when the player is grappling or not.
    
    Also altered project settings to make the game more narrow to fit its vertical movement, and maintain its viewport regardless of screen size.
    
    Next intended step is implementation of the grappling hook physics and visuals.

--- code/player.gd
+++ code/player.gd
@@ -3,8 +3,9 @@ extends CharacterBody2D
 var GRAVITY = 50
 
 var mouse_position
+var grappling = false
 
-@onready var pointer = $FallingForItPointer
+@onready var pointer = $Pointer
 
 # Code that runs at the start of the game
 func _ready():
@@ -15,8 +16,22 @@ func _physics_process(delta):
 	mouse_position = get_global_mouse_position()
 	
 	_animate(mouse_position)
+	
 	#Adjust for gravity
-	#velocity.y += GRAVITY
+	velocity.y += GRAVITY
+	
+	#Handle mouse click for grapple. 
+	if !grappling:
+		#Player begins grapple, grappling is true
+		if Input.is_action_just_pressed("click"):
+			grappling = true
+	else:
+		#Grapple is released, grapple is false
+		if Input.is_action_just_released("click"):
+			grappling = false
+	
+	
+	move_and_slide()
 	
 func _animate(mouse_position):
 	
