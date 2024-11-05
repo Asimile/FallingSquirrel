6ddbd6ec8ccdde773e6f934c8c9dc50f3a10b0b2
Asimile <AsimNRaja@gmail.com>
2024-11-04 22:36:49 -0500

    Swing physics implemented
    Swinging mechanic completely implemented. Had to switch PlayerBody2D to a StaticBody2D node to make it fall under the engine physics. Quick fix, maybe one day I figure it out for a playerbody. Unfortunately I cannot figure out how to make the player sprite not rotate with the whole static body. To be figured out.
    
    Ancillary: Tilemap is ready to paint

--- code/player.gd
+++ code/player.gd
@@ -1,11 +1,10 @@
-extends CharacterBody2D
+extends RigidBody2D
 
-var GRAVITY = 30
+#var GRAVITY = 30
 var JUMP_STRENGTH = 800
 
 var mouse_position
 var hooked = false
-var airborne = false
 
 @onready var pointer = $Pointer
 @onready var hookRaycast = $RayCast2D
@@ -21,45 +20,59 @@ func _ready():
 
 # This is code that runs every single frame
 func _physics_process(delta):
+	$Sprite2D.rotation = 0
+	
 	mouse_position = get_global_mouse_position()
 	
 	if Input.is_action_just_pressed("click") and not hooked:
-		hooked = true
-		
-		hookRaycast.target_position = to_local(get_global_mouse_position())
-		hookRaycast.force_raycast_update()
-		if hookRaycast.is_colliding():
-			# Get values from raycast
-			var hook_pos = hookRaycast.get_collision_point()
-			var collider = hookRaycast.get_collider()
-			# If the ray collides with a hookable object, move pinjoint and hook to it
-			if collider.is_in_group("Hookable"):
-				pinjoint.global_position = hook_pos
-				hook.global_position = hook_pos
-				pinjoint.node_b = get_path_to(hook)
-				# Rotate the hook so it is the right angle (If I was giving it a sprite)
-				hook.rotation = (hook_pos - global_position).angle() # Where in parentheses is a direction, essentially
-				
-				
+		if mouse_position.y <= position.y:
+			hooked = true
+			
+			hookRaycast.target_position = to_local(get_global_mouse_position()) * 10
+			hookRaycast.force_raycast_update()
+			if hookRaycast.is_colliding():
+				# Get values from raycast
+				var hook_pos = hookRaycast.get_collision_point()
+				var collider = hookRaycast.get_collider()
+				# If the ray collides with a hookable object, move pinjoint and hook to it
+				if collider.is_in_group("Hookable"):
+					pinjoint.global_position = hook_pos
+					hook.global_position = hook_pos
+					pinjoint.node_b = get_path_to(hook)
+					# Rotate the hook so it is the right angle (If I was giving it a sprite)
+					hook.rotation = (hook_pos - global_position).angle() # Where in parentheses is a direction, essentially
+	elif Input.is_action_just_released("click") and hooked:
+			hooked = false
+			pinjoint.node_b = NodePath("")
+	
+	if hooked:
+		line.clear_points()
+		line.add_point(Vector2.ZERO)
+		line.add_point(to_local(line_end.global_position))
+	else:
+		line.clear_points()
 	
 	_animate(mouse_position)
 	
-	#Adjust for gravity
-	velocity.y += GRAVITY
+	# Physics code from video
+	var grounded = get_contact_count()>0
 	
-	# Bounce Logic
-	#if airborne and is_on_floor():
-		#velocity.y = -velocity.y * .5
-		#airborne = false
+	#if Input.is_action_pressed("retract") and hooked:
+		#retract code here
+		
+	#basic platformer code below
+	if Input.is_action_pressed("right") and (hooked):
+		apply_central_impulse(Vector2.RIGHT)
+		apply_central_impulse(Vector2.RIGHT)
+	if Input.is_action_pressed("left") and (hooked):
+		apply_central_impulse(Vector2.LEFT)
+		apply_central_impulse(Vector2.LEFT)
+	if Input.is_action_just_pressed("jump") and grounded:
+		apply_central_impulse(Vector2.UP * 400)
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = -JUMP_STRENGTH
-		#airborne = true
-		print("Jumped")
+	#move_and_slide()
 	
-	
-	move_and_slide()
-	
+
 func _animate(mouse_position):
 	
 	# Grapple pointer points where player aims with mouse
